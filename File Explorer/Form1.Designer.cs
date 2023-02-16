2f44352189c287b41cd9daad0979ebad01185bee
Light-Dragon-0 <ayaadm730@gmail.com>
2023-02-16 12:25:57 +0200

    Add project files.

---
+++ File Explorer/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace File_Explorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem6 = new System.Windows.Forms.ListViewItem("");
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(32, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "<";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(32, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(32, 32);
+            this.button2.TabIndex = 1;
+            this.button2.Text = ">";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(64, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(32, 32);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "^";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(96, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(488, 30);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(588, 0);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(210, 30);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Enter += new System.EventHandler(this.textBox2_Enter);
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.HideSelection = false;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem6});
+            this.listView1.Location = new System.Drawing.Point(0, 36);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(798, 413);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.DockChanged += new System.EventHandler(this.listView1_DockChanged);
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ File Explorer/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace File_Explorer
+{
+    public partial class Form1 : Form
+    {
+        Stack<string> trace = new Stack<string>();
+        Stack<string> rtrace = new Stack<string>();
+
+        string currentDir = @"D:\";
+        public Form1()
+        {
+            InitializeComponent();
+            textBox2.GotFocus += TextBox2_GotFocus;
+            textBox2.LostFocus += TextBox2_LostFocus;
+        }
+
+        private void TextBox2_LostFocus(object sender, EventArgs e)
+        {
+            if(textBox2.Text == "")
+            textBox2.Text = "Search";
+        }
+
+        private void TextBox2_GotFocus(object sender, EventArgs e)
+        {
+            if(textBox2.Text == "Search")
+            this.textBox2.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadDirectories();
+        }
+        private void LoadDirectories()
+        {
+            if (System.IO.Directory.Exists(currentDir)) { 
+                this.listView1.Items.Clear();
+                string[] dirs = System.IO.Directory.GetDirectories(currentDir);
+                foreach(string dir in dirs) this.listView1.Items.Add(new ListViewItem(dir.Split('\\').Last()) { BackColor = Color.Yellow } );
+                string[] files = System.IO.Directory.GetFiles(currentDir);
+                foreach (string file in files) this.listView1.Items.Add(new ListViewItem(file.Split('\\').Last()) { BackColor = Color.Blue,ForeColor = Color.White });
+                this.textBox1.Text = currentDir;
+            }
+            else if(System.IO.File.Exists(currentDir.Remove(currentDir.Length -1,1)))
+            {
+                System.Diagnostics.Process.Start(currentDir);
+                Previous();
+            }
+        }
+        private void Previous()
+        {
+            if (trace.Count <= 0)
+                return;
+            rtrace.Push(currentDir);
+            currentDir = trace.Pop();
+            LoadDirectories();
+        }
+        private void Forward()
+        {
+            if (rtrace.Count <= 0)
+                return;
+            trace.Push(currentDir);
+            currentDir = rtrace.Pop();
+            LoadDirectories();
+        }
+        private void Back()
+        {
+            string[] r = currentDir.Split('\\');
+            if (r.Length > 1)
+                rtrace.Push(currentDir);
+            currentDir = currentDir.Replace("\\" + currentDir.Split('\\')[r.Length - 2] + "\\", "\\");
+            LoadDirectories();
+        }
+        private void listView1_DockChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            trace.Push(currentDir);
+            currentDir += this.listView1.SelectedItems[0].Text + '\\';
+            LoadDirectories();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Previous();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Back();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)13)
+            {
+                if(System.IO.Directory.Exists(this.textBox1.Text))
+                {
+                    trace.Push(currentDir);
+                    if (textBox1.Text.Last() != '\\')
+                        textBox1.Text += '\\';
+                    currentDir = textBox1.Text;
+                    LoadDirectories();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Forward();
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            MessageBox.Show("WOOOWWWW ARE YOU REALLY TRYING TO SEARCH SOMETHING HERE ??!!!!!");
+        }
+    }
+}
